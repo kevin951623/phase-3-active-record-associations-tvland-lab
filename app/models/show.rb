62852f753e7777c77c38d76bd794dcdfe51aac06
Kevin Mak <kmak1129@gmail.com>
2023-01-18 18:02:23 -0500

    commit tree 896ccaa5470ec3f90dbe413420dd05b9fc0688c8

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has many :actors, through: :characters
+
+  def actors_list
+    self.actors.map do |a|
+        "#{a.full_name}"
+    end
+  end 
+
 end
